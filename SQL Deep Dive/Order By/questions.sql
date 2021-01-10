aaa559abcb0a62a5d1d73bf569b6b85e00c01ace
kittuData <70167469+kittuData@users.noreply.github.com>
2021-01-10 21:57:53 +0530

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,17 +3,21 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
-
+select first_name,last_name from employees
+order by first_name asc,last_name desc;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees by age
 */
-
+select age from employees
+order by age desc;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+select first_name from employees
+where first_name like 'k%' order by hire_date;
